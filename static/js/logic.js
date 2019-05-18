912501b97e05dbeb12224d2f9758f4f1c6b4317e
Ruzzo <eric.ruzzo@gmail.com>
2019-05-18 16:28:46 -0400

    completed legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,5 @@
-function createMap(bikeStations) {
+// Function to initialize map
+function createMap(quakeLocations) {
 
   // Create the tile layer that will be the background of our map
   var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
@@ -13,50 +14,124 @@ function createMap(bikeStations) {
     "Light Map": lightmap
   };
 
-  // Create an overlayMaps object to hold the bikeStations layer
+  // Create an overlayMaps object to hold the quakeLocations layer
   
   var overlayMaps = {
-    "Bike Stations": bikeStations
+    "Earthquake Locations": quakeLocations
   };
 
   // Create the map object with options
   var map = L.map("map-id", {
-    center: [40.73, -74.0059],
-    zoom: 12,
-    layers: [lightmap]
+    center: [36.1699, -115.1398],
+    zoom: 5,
+    layers: [lightmap, quakeLocations]
   });
 
   // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
-    collapsed: true
+    collapsed: false
   }).addTo(map);
+  
+  // Pass in createLegend function to include legend in layer control
+  createLegend(map);
 }
 
+// Function to assign colors to legend
+// Colors based on colors in createMarkers function
+function getColor(d) {
+  return d > 5  ? '#ff4500' :
+         d > 4  ? '#f77802' :
+         d > 3  ? '#ed9f0c' :
+         d > 2   ? '#dec117' :
+         d > 1   ? '#cae123' :
+         d > 0   ? '#adff2f' :
+                    '#FFEDA0';
+}
+
+// Function to create legend
+function createLegend(map) {
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = [0, 1, 2, 3, 4, 5];
+    var labels = [];
+
+    // Create title for legend and pass in to html
+    var legendInfo = "<b>Magnitude</b><br>";
+
+    div.innerHTML = legendInfo;
+
+    // Loop through limits, using getColor function to add colors
+    for (var i = 0; i < limits.length; i++) {
+      div.innerHTML +=
+          '<i style="background-color:' + getColor(limits[i] + 1) + '"></i> ' +
+          limits[i] + (limits[i + 1] ? '&ndash;' + limits[i + 1] + '<br>' : '+');
+  }
+    return div;
+};
+  // Add legend to map
+  legend.addTo(map);
+};
+
+// Function to create earthquake markers
 function createMarkers(response) {
-  console.log(response);
 
-  // Pull the "stations" property off of response.data
-  var stations = response.data.stations;
+  // Pull features from response
+  var earthquakes = response.features;
 
-  // Initialize an array to hold bike markers
-  var bikeMarkers = [];
+  // Initialize an array to hold earthquake markers
+  var quakeMarkers = [];
 
-  // Loop through the stations array
-  for (var index = 0; index < stations.length; index++) {
-    var station = stations[index];
+  // Loop through features
+  for (var index = 0; index < earthquakes.length; index++) {
+    
+    // Set variable for each earthquake
+    var quake = earthquakes[index];
+    
+    // Determine magnitude of each earthquake and assign to variable
+    var magnitude = quake.properties.mag;
+    
+    // Initialize circleColor variable
+    var circleColor = "";
 
-    // For each station, create a marker and bind a popup with the station's name
-    var bikeMarker = L.marker([station.lat, station.lon])
-      .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "<h3>");
+    // If statement to determine color of marker based on magnitude
+    if (magnitude > 5) {
+      var circleColor = "#ff4500"
+    }
+    else if (magnitude > 4) {
+      var circleColor = "#f77802"
+    }
+    else if (magnitude > 3) {
+      var circleColor = "#ed9f0c"
+    }
+    else if (magnitude > 2) {
+      var circleColor = "#dec117"
+    }
+    else if (magnitude > 1) {
+      var circleColor = "#cae123"
+    }
+    else {
+      var circleColor = "#adff2f"
+    }
 
-    // Add the marker to the bikeMarkers array
-    bikeMarkers.push(bikeMarker);
+    // Create circle marker for each earthquake
+    var quakeMarker = L.circle(
+      [quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {radius: magnitude * 20000, 
+        color: "black", 
+        weight: 1, 
+        fillColor: circleColor, 
+        fillOpacity: 0.8})
+      
+        // Bind popup w/ location, time and magnitude of earthquake
+        .bindPopup("<h3>" + quake.properties.place + "<h3><hr><p>" + new Date(quake.properties.time) + "<br>" + "Magnitude: " + magnitude + "</p");
+
+    // Add each marker to the quakeMarkers array
+    quakeMarkers.push(quakeMarker);
   }
 
-  // Create a layer group made from the bike markers array, pass it into the createMap function
-  createMap(L.layerGroup(bikeMarkers));
-}
+  // Create a layer group made from the quakeMarkers array and pass into the createMap function
+  createMap(L.layerGroup(quakeMarkers));
+};
 
-
-// Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
-d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json", createMarkers);
+// Call USGSS earthquake API to find info for all earthquakes in the last week. Call createMarkers when complete
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", createMarkers);
